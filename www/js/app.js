069334f71a96dcd356d7b32efc3b8adbc5d666d3
aniansson <ebba@aniansson.se>
2017-01-20 12:58:45 +0100

    Add controller for test

--- www/js/app.js
+++ www/js/app.js
@@ -39,6 +39,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
           templateUrl: 'templates/about/about.html'
         }
       }
+    })
+
+    .state('app.test', {
+      url:'/test',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/test/test.html',
+          controller: 'TestController'
+        }
+      },
     });
 
   // if none of the above states are matched, use this as the fallback
